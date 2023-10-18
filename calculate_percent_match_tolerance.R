1bdd537d20af71d8520fd45241f15e584f79df1f
Alicia Vallorani <auv27@psu.edu>
2023-10-18 10:53:28 -0400

    edit number of seconds

--- calculate_percent_match_tolerance.R
+++ calculate_percent_match_tolerance.R
@@ -3,11 +3,11 @@ calculate_percent_match_tolerance <- function(rater1, rater2) {
   match_results <- logical(length(rater1))
   
   # Check if each value in rater 1 has a match within tolerance in rater 2
-  for (i in 1:length(rater1)) {
+  for (i in 3:length(rater1)) {
     value_to_match <- rater1[i]
     
-    # Check if there is a match within ±1 row (±1 second) in rater 2
-    if (value_to_match %in% rater2[(i - 1):(i + 1)]) {
+    # Check if there is a match within ±3 row (±3 seconds) in rater 2
+    if (value_to_match %in% rater2[(i - 3):(i + 3)]) {
       match_results[i] <- TRUE
     }
   }
